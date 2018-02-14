f6be3d7c944801d24b5903af0b9bfb3a7752cba2
ArinaAnderson <iolsoOfuolso@gmail.com>
2018-02-14 04:39:17 -0800

    travis check

--- source/js/picturefill.js
+++ source/js/picturefill.js
@@ -1438,8 +1438,6 @@
         picturefill = noop;
         pf.fillImg = noop;
     } else {
-
-         // Set up picture polyfill by polling the document
         (function() {
             var isDomReady;
             var regReady = window.attachEvent ? /d$|^c/ : /d$|^c|^i/;
